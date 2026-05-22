e3cfd0b2d761173ceef13992ac684036ce5b7832
gabes19 <xxc5sg@virginia.edu>
2026-05-22 17:30:14 -0400

    File upload functionality

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
-from flask import Flask
+import os
+
+from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
+
+
 app = Flask(__name__)
+
+#Upload folder for local dev
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
 @app.route("/")
 def start():
-    return "<p>Start page</p>"
+    return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    '''Handles user CSV upload'''
+    uploaded_file = request.files.get("csv_file")
+
+    if uploaded_file is None or uploaded_file.filename == "":
+        return "No file uploaded", 400
+    
+    if not uploaded_file.filename.endswith(".csv"):
+        return "Please upload a CSV file", 400
+    
+    filename = secure_filename(uploaded_file.filename)
+    save_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+
+    uploaded_file.save(save_path)
+
+    return f"Uploaded {filename} succesffuly"
